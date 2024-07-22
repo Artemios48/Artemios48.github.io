b49f727bac46c246e054979e61fe5c12bcf2850e
Artemios48 <149158649+Artemios48@users.noreply.github.com>
2024-07-22 18:21:28 +0300

    Add files via upload

---
+++ Dima.js
@@ -0,0 +1,37 @@
+const urlParams = new URLSearchParams(window.location.search);
+let tg = window.Telegram.WebApp;
+let letsgo = document.getElementById('letsgo_btn');
+let click = document.getElementById('click');
+let a = urlParams.get('c');
+let s = urlParams.get('s');
+let data = {}
+const $button_cl = document.querySelector('.button_cl')
+
+
+function setImage(){
+    if (s == 'normal') {
+        $button_cl.setAttribute('style', 'background-image: url("normal.png")')
+    }
+    else if (s == 'rock') {
+        $button_cl.setAttribute('style', 'background-image: url("rock.png")')
+    }
+    else if (s == 'buisness') {
+        $button_cl.setAttribute('style', 'background-image: url("buisness.png")')
+    }
+    else if (s == 'hack') {
+        $button_cl.setAttribute('style', 'background-image: url("hacker.png")')
+    }
+}
+setImage()
+click.addEventListener('click', () => {
+    a++;
+    document.getElementById('clicks').innerText = a;
+});
+
+letsgo.addEventListener('click', () => {
+    let data = {
+        a: a
+    }
+    tg.sendData(JSON.stringify(data));
+    tg.close();
+});
